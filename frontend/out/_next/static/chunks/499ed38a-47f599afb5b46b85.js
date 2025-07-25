657b2ab17978abd71c6ec966641840ac902a42f4
bekfastbek <kinshukkataria4@gmail.com>
2025-07-26 01:02:09 +0530

    added build script in dfx.json

--- frontend/out/_next/static/chunks/499ed38a-47f599afb5b46b85.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[618],{7186:function(t,n,u){u.d(n,{VzJ:function(){return c}});var a=u(8516);function c(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"m16 464 480-208L16 48v160l320 48-320 48z"},child:[]}]})(t)}}}]);
